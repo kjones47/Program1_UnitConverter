0d5f5bfde3cf653c5778c77dffd7a6a3c9c7c857
kjones47 <47005337+kjones47@users.noreply.github.com>
2019-09-02 22:36:00 -0400

    upload the edited files, java and xml
     the whole app build is too large for git

---
+++ MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.android.cs441_proj1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int tempF, tempC;
+        //VARIABLES
+        final TextView tv = (TextView) findViewById(R.id.text1);
+        final EditText temp1 = (EditText) findViewById(R.id.temp1);
+        final EditText temp2 = (EditText) findViewById(R.id.temp2);
+        temp1.addTextChangedListener((new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                String degF = temp1.getText().toString();
+                int todegreesC = (Integer.parseInt(degF) - 32) * 5/9;
+                temp2.setText(degF);
+            }
+        }));
+        temp2.addTextChangedListener((new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                String degC = temp2.getText().toString();
+                int todegreesF = (Integer.parseInt(degC) * (9/5)) + 32;
+                temp1.setText(todegreesF);
+            }
+        }));
+
+        EditText zipcode = (EditText) findViewById(R.id.zipcode);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("BUTTON CLICKED", null).show();
+                tv.setText("Hello again");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
