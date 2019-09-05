0d34084dccbd6bee7dd4b6f7445341a672073bd9
kjones47 <47005337+kjones47@users.noreply.github.com>
2019-09-04 23:41:51 -0400

    weather app files

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,16 @@
-package com.example.android.cs441_proj1;
+package com.example.android.weatherapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,57 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        int tempF, tempC;
-        //VARIABLES
-        final TextView tv = (TextView) findViewById(R.id.text1);
-        final EditText temp1 = (EditText) findViewById(R.id.temp1);
-        final EditText temp2 = (EditText) findViewById(R.id.temp2);
-        temp1.addTextChangedListener((new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {}
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                String degF = temp1.getText().toString();
-                int todegreesC = (Integer.parseInt(degF) - 32) * 5/9;
-                temp2.setText(degF);
-            }
-        }));
-        temp2.addTextChangedListener((new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {}
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                String degC = temp2.getText().toString();
-                int todegreesF = (Integer.parseInt(degC) * (9/5)) + 32;
-                temp1.setText(todegreesF);
-            }
-        }));
-
-        EditText zipcode = (EditText) findViewById(R.id.zipcode);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("BUTTON CLICKED", null).show();
-                tv.setText("Hello again");
+                Snackbar.make(view, "Welcome to the weather app", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
     }
 
     @Override
